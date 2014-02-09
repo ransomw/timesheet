a702a7d261bac210a3439be77460b4c810210f4a
ransom <auvergnerw@gmail.com>
2014-02-09 12:58:50 +0800

    ah, lobos

--- timesheet-server/src/timesheet_server/core.clj
+++ timesheet-server/src/timesheet_server/core.clj
@@ -2,8 +2,13 @@
 		(:gen-class :main true)
 		(:use compojure.core)
 		(:use korma.core)
+		(:use korma.db)
 		;; (:use clojure.java.jdbc)
-		(:require [compojure.route :as route]))
+		(:require [compojure.route :as route])
+		(:require [lobos.core :as lcore])
+		(:require [lobos.connectivity :as lconn])
+		(:require [lobos.schema :as ls])
+		)
 
 ;; (def db
 ;;   {:classname   "org.sqlite.JDBC"
@@ -11,9 +16,18 @@
 ;;    :subname     "/tmp/timesheet.sqlite"
 ;;   })
 
-(use 'korma.db)
-
 (def db (sqlite3 {:db "/tmp/timesheet.sqlite"}))
+
+;; (use '(lobos connectivity core schema))
+
+(lconn/open-global db)
+
+(lcore/create
+ (ls/table :entry
+	 (ls/integer :id :primary-key)
+	 (ls/varchar :date 10)
+	 (ls/varchar :time 10)
+	 (ls/varchar :comment 100)))
 
 (defdb korma-db db)
 
@@ -26,7 +40,7 @@
 	(entity-fields :date :time :comment)
 	)
 
-;; (insert entry (values {:date "today" :time "now" :comment "this"}))
+(insert entry (values {:date "today" :time "now" :comment "this"}))
 
 (defroutes app
 	(GET "/" [] "<h1>Hello World</h1>")
