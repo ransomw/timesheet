e79910f644cfae744f0ff69f4703d117716f5f52
ransom <auvergnerw@gmail.com>
2014-02-15 16:46:42 +0800

    move database stuffs into seperate namespace

--- timesheet-server/src/timesheet_server/core.clj
+++ timesheet-server/src/timesheet_server/core.clj
@@ -1,87 +1,13 @@
 (ns timesheet-server.core
-		(:gen-class :main true)
-		(:use compojure.core)
-		(:use korma.core)
-		(:use korma.db)
-		(:use clojure.java.io)
-		;; (:use clojure.java.jdbc)
-		(:require [compojure.route :as route])
-		(:require [lobos.core :as lcore])
-		(:require [lobos.connectivity :as lconn])
-		(:require [lobos.schema :as ls])
-		(:require [clojure.string :as string])
-		)
+  (:gen-class :main true)
+  (:use compojure.core)
+  (:use clojure.java.io)
+  ;; (:use clojure.java.jdbc)
+  (:require [compojure.route :as route])
+  (:require [timesheet-server.db :as db])
+  )
 
-;; (def db
-;;   {:classname   "org.sqlite.JDBC"
-;;    :subprotocol "sqlite"
-;;    :subname     "/tmp/timesheet.sqlite"
-;;   })
-
-(def db (sqlite3 {:db "/tmp/timesheet.sqlite"}))
-
-;; (use '(lobos connectivity core schema))
-
-(lconn/open-global db)
-
-(lcore/create
- (ls/table :entry
-	 (ls/integer :id :primary-key)
-	 (ls/varchar :date 10)
-	 (ls/varchar :time 10)
-	 (ls/varchar :project 20)
-	 (ls/varchar :category 20)
-	 (ls/varchar :comment 1000)))
-
-(defdb korma-db db)
-
-(declare entry)
-
-(defentity entry
-	;; (pk :id)
-	;; (table :entry)
-	;; (database db)
-	(entity-fields :date :time :project :category :comment)
-	)
-
-;; (insert entry (values {:date "entry-date"
-;; 											:time "entry-time"
-;; 											:project "entry-project"
-;; 											:category "entry-category"
-;; 											:comment "entry-comment"}))
-
-(defn store-text-file-entry [entry-date text-file-entry]
-	(let [split-entry (string/split text-file-entry #"\n")]
-		(let [entry-time (first split-entry)
-				 entry-project (first (rest split-entry))
-				 entry-category (first (rest (rest split-entry)))
-				 entry-comment (rest (rest (rest split-entry)))]
-		(insert entry (values {:date entry-date
-													:time entry-time
-													:project entry-project
-													:category entry-category
-													:comment (format "%s" entry-comment) }))
-
-)))
-
-(defn store-text-file-date-entry [date-entry]
-	(let [split-entry (string/split date-entry #"\n#\n")]
-			 (let [entry-date (string/trim (first split-entry))
-						text-file-entries (rest split-entry)]
-						(doseq [text-file-entry text-file-entries]
-									 (store-text-file-entry entry-date text-file-entry))
-						)))
-
-(defn load-text-file [text-file-path]
-	(let [timesheet-text-file (slurp text-file-path)]
-		(let [text-file-date-entries (filter (fn [entry] (not (= entry "")))
-			     (string/split (get
-			       (string/split timesheet-text-file #"\n\f\f\n") 0 nil) #"\f"))]
-		  (doseq [text-file-date-entry text-file-date-entries]
-						 (store-text-file-date-entry text-file-date-entry ))
-			)))
-
-(load-text-file "/tmp/timesheet.txt")
+(db/load-text-file "/tmp/timesheet.txt")
 
 (defroutes app
 	(GET "/" [] "<h1>Hello World</h1>")
