7f2d808b4bd6dcb3d2f2efc7bbfffadd0a2390b9
ransom <auvergnerw@gmail.com>
2014-02-17 17:32:55 +0800

    hey, om

--- timesheet-client/src/timesheet_client/core.cljs
+++ timesheet-client/src/timesheet_client/core.cljs
@@ -1,5 +1,15 @@
-(ns timesheet-client.core)
+(ns timesheet-client.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
 
 (enable-console-print!)
 
-(println "Hello world!")
+;; (println "Hello world!")
+
+(def app-state (atom {:text "Hello world!"}))
+
+(om/root
+  (fn [app owner]
+    (dom/h1 nil (:text app)))
+  app-state
+  {:target (. js/document (getElementById "app"))})
