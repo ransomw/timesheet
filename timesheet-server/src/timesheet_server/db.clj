ac99d0d50eb24798627cf9002f42e703be73fba4
ransom <auvergnerw@gmail.com>
2014-02-17 21:31:44 +0800

    sequential (let

--- timesheet-server/src/timesheet_server/db.clj
+++ timesheet-server/src/timesheet_server/db.clj
@@ -54,36 +54,36 @@
 ;; 											:comment "entry-comment"}))
 
 (defn store-text-file-entry [entry-date text-file-entry]
-	(let [split-entry (string/split text-file-entry #"\n")]
-		(let [entry-time (first split-entry)
-          entry-project (first (rest split-entry))
-          entry-category (first (rest (rest split-entry)))
-          entry-comment (rest (rest (rest split-entry)))]
-      (insert entry (values {:date entry-date
-                             :time entry-time
-                             :project entry-project
-                             :category entry-category
-                             :comment (string/join "\n" entry-comment) }))
-      )))
+	(let [split-entry (string/split text-file-entry #"\n")
+        entry-time (first split-entry)
+        entry-project (first (rest split-entry))
+        entry-category (first (rest (rest split-entry)))
+        entry-comment (rest (rest (rest split-entry)))]
+    (insert entry (values {:date entry-date
+                           :time entry-time
+                           :project entry-project
+                           :category entry-category
+                           :comment (string/join "\n" entry-comment) }))
+    ))
 
 (defn store-text-file-date-entry [date-entry]
-	(let [split-entry (string/split date-entry #"\n#\n")]
-    (let [entry-date (string/trim (first split-entry))
-          text-file-entries (rest split-entry)]
-      (doseq [text-file-entry text-file-entries]
-        (store-text-file-entry entry-date text-file-entry))
-      )))
+	(let [split-entry (string/split date-entry #"\n#\n")
+        entry-date (string/trim (first split-entry))
+        text-file-entries (rest split-entry)]
+    (doseq [text-file-entry text-file-entries]
+      (store-text-file-entry entry-date text-file-entry))
+    ))
 
 (defn load-text-file [text-file-path]
-	(let [timesheet-text-file (slurp text-file-path)]
-		(let [text-file-date-entries
-          (filter (fn [entry] (not (= entry "")))
-                  (string/split
-                   ((string/split timesheet-text-file #"\n\f\f\n") 0)
-                   #"\f"))]
-		  (doseq [text-file-date-entry text-file-date-entries]
-        (store-text-file-date-entry text-file-date-entry ))
-			)))
+	(let [timesheet-text-file (slurp text-file-path)
+        text-file-date-entries
+        (filter (fn [entry] (not (= entry "")))
+                (string/split
+                 ((string/split timesheet-text-file #"\n\f\f\n") 0)
+                 #"\f"))]
+    (doseq [text-file-date-entry text-file-date-entries]
+      (store-text-file-date-entry text-file-date-entry ))
+    ))
 
 (defn get-entries []
   (select entry))
