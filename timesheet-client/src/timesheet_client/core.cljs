ead5c2ee47c63487281c7aa0cb2b7dd832294650
ransom <auvergnerw@gmail.com>
2014-04-07 18:13:53 +0800

    render a list of things

--- timesheet-client/src/timesheet_client/core.cljs
+++ timesheet-client/src/timesheet_client/core.cljs
@@ -6,10 +6,23 @@
 
 ;; (println "Hello world!")
 
-(def app-state (atom {:text "Hello world!"}))
+(def app-state (atom
+                {:text "Hello world!"
+                 :time-by-day
+                 [
+                  {:day "monday" :time 4}
+                  {:day "tuesday" :time 2}
+                  {:day "wednesday" :time 6}
+                  ]
+                 }))
 
 (om/root
   (fn [app owner]
-    (dom/h1 nil (:text app)))
+    ;; (dom/h1 nil (:text app))
+    (apply dom/ul nil
+           (map (fn [text] (dom/li nil text))
+                (map (fn [entry] (:day entry))
+                       (:time-by-day app))))
+    )
   app-state
   {:target (. js/document (getElementById "app"))})
